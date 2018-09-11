611e67ae2a5b509fe3828d7f99491f7ae60fc833
Chris <chrischen1@users.noreply.github.com>
2018-09-10 20:10:22 -0500

    Update plot_utils.R

--- plot_utils.R
+++ plot_utils.R
@@ -288,15 +288,15 @@ plot_chromesome <- function(ratio,chr){
 }
 
 plot_vol_edgeR <- function(counts,grp,plot_title='',hide_black_dots =F,cex=1,show_lines=T,pval_plot=F,gene_keep=NULL){
-  de1 <- edgeR_wrapper(counts[,rownames(grp)],grp)
+  de1 <- data.frame(edgeR_wrapper(counts[,rownames(grp)],grp)[[1]])
   if(!is.null(gene_keep)){
     de1 <- de1[gene_keep,]
     counts <- counts[gene_keep,]
   }
   if(!pval_plot){
-    plot_volcano(Fold_Change = 2^(de1[,3]),P_Value = de1[,1],show_lines = show_lines,hide_black_dots = hide_black_dots,cex=cex,Expre = apply(counts, 1, mean),plot_title=plot_title)
+    plot_volcano(Fold_Change = 2^(de1$logFC),P_Value = de1$FDR,show_lines = show_lines,hide_black_dots = hide_black_dots,cex=cex,Expre = apply(counts, 1, mean),plot_title=plot_title)
   }else{
-    plot_volcano_pval(Fold_Change = 2^(de1[,3]),P_Value = de1[,1],show_lines = show_lines,hide_black_dots = hide_black_dots,cex=cex,plot_title=plot_title)
+    plot_volcano_pval(Fold_Change = 2^(de1$logFC),P_Value = de1$FDR,show_lines = show_lines,hide_black_dots = hide_black_dots,cex=cex,plot_title=plot_title)
     
   }
 }
