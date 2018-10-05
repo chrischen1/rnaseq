e381d11838a66468a39ed617a4256aa89f82aeca
Chris <chrischen1@users.noreply.github.com>
2018-10-05 17:21:19 -0500

    Create chipseq_test.R

---
+++ chipseq_test.R
@@ -0,0 +1,44 @@
+system('module load bowtie2/bowtie2-2.2.9')
+system('module load trimmomatic/trimmomatic-0.36')
+system('module load fastqc')
+
+raw_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data/'
+trim_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data_trimmed/'
+qc_result_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data_qc/'
+bam_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/bams/'
+log_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/logs/'
+
+ref_genome_path = '/storage/htc/bdm/ref_genomes/B73/index_bowtie2'
+dir.create(trim_data_path)
+dir.create(qc_result_path)
+dir.create(bam_data_path)
+dir.create(log_path)
+
+setwd(log_path)
+
+#1.Trimming adapter and Filter low quality reads(Hua not run)
+all_samples <- unique(gsub('_R.+','',list.files(raw_data_path)))
+for(i in all_samples){
+  infile1 <- paste0(raw_data_path,i,'_R1_001.fastq.gz')
+  infile2 <- paste0(raw_data_path,i,'_R2_001.fastq.gz')
+  out_files <- paste0(trim_data_path,i,
+                      c('_R1_paired.fastq.gz','_R1_unpaired.fastq.gz','_R2_paired.fastq.gz','_R2_unpaired.fastq.gz')
+                      ,collapse = ' ')
+  system(paste('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch java -jar /cluster/software/Trimmomatic/Trimmomatic-0.36/trimmomatic-0.36.jar PE',infile1,infile2,
+               out_files,'LEADING:30 TRAILING:29 SLIDINGWINDOW:4:30 MINLEN:29 AVGQUAL:28'))
+}
+#2. QC with fastqc
+for(i in list.files(trim_data_path)){
+  system(paste0('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch fastqc ',
+                trim_data_path,i,' -o ',qc_result_path))
+}
+
+#3. Map reads to genome (B73 + B cenromere sequences, should bind together)
+for (i in all_samples) {
+  infile1 <- paste0(trim_data_path,i,'_R1_paired.fastq.gz')
+  infile2 <- paste0(trim_data_path,i,'_R2_paired.fastq.gz')
+  outfile <- paste0(bam_data_path,i,'.sam')
+  system(paste('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch bowtie2 -x ',ref_genome_path,
+               '-1',infile1,'-2',infile2,'-S',outfile))
+}
+#4. MACS2 call peaks 
