97c324314fc7c954861b1141fbe771016fe32615
Chris <chrischen1@users.noreply.github.com>
2018-10-08 15:41:08 -0500

    Update chipseq_test.R

--- chipseq_test.R
+++ chipseq_test.R
@@ -1,6 +1,5 @@
 # environment set
 raw_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data/'
-trim_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data_trimmed/'
 filter_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data_trimmed_filtered/'
 qc_result_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/ChIP_data_qc/'
 sam_data_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/sams/'
@@ -10,7 +9,6 @@ bam_merge_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/data/bams_merge/'
 log_path = '/storage/htc/birchlerlab/CENH3_ChIP-seq/logs/'
 
 ref_genome_path = '/storage/htc/bdm/ref_genomes/B73/index_bowtie2'
-dir.create(trim_data_path)
 dir.create(qc_result_path)
 dir.create(sam_data_path)
 dir.create(bam_data_path)
@@ -21,24 +19,10 @@ dir.create(log_path)
 all_samples <- unique(gsub('_R.+','',list.files(raw_data_path)))
 setwd(log_path)
 
-#1.Trimming adapter and Filter low quality reads
+#1.Filter low quality reads
 for(i in all_samples){
   infile1 <- paste0(raw_data_path,i,'_R1_001.fastq.gz')
   infile2 <- paste0(raw_data_path,i,'_R2_001.fastq.gz')
-  out_file1 <- paste0(trim_data_path,i,'_R1_001.fastq.gz')
-  out_file2 <- paste0(trim_data_path,i,'_R2_001.fastq.gz')
-  adapter_seq <- unlist(strsplit(i,'_'))[3]
-  out_files <- paste0(trim_data_path,i,
-                      c('_R1_paired.fastq.gz','_R1_unpaired.fastq.gz','_R2_paired.fastq.gz','_R2_unpaired.fastq.gz')
-                      ,collapse = ' ')
-  system(paste('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch cutadapt -a',
-               adapter_seq,'-A',adapter_seq,'-o',out_file1,'-p',out_file2,infile1,infile2))
-}
-
-#2.Filter low quality reads
-for(i in all_samples){
-  infile1 <- paste0(trim_data_path,i,'_R1_001.fastq.gz')
-  infile2 <- paste0(trim_data_path,i,'_R2_001.fastq.gz')
   out_files <- paste0(filter_data_path,i,
                       c('_R1_paired.fastq.gz','_R1_unpaired.fastq.gz','_R2_paired.fastq.gz','_R2_unpaired.fastq.gz')
                       ,collapse = ' ')
@@ -46,13 +30,13 @@ for(i in all_samples){
                out_files,'LEADING:30 TRAILING:29 SLIDINGWINDOW:4:30 MINLEN:29 AVGQUAL:28'))
 }
 
-#3. QC with fastqc
+#2. QC with fastqc
 for(i in list.files(filter_data_path,full.names = T)){
   system(paste0('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch fastqc ',
                 i,' -o ',qc_result_path))
 }
 
-#4. Map reads to genome (B73 + B cenromere sequences, should bind together) and merge bam files for replicates
+#3. Map reads to genome (B73 + B cenromere sequences, should bind together) and merge bam files for replicates
 for (i in all_samples) {
   infile1 <- paste0(filter_data_path,i,'_R1_paired.fastq.gz')
   infile2 <- paste0(filter_data_path,i,'_R2_paired.fastq.gz')
@@ -61,14 +45,14 @@ for (i in all_samples) {
   system(paste('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch bowtie2 -x ',ref_genome_path,
                '-1',infile1,'-2',infile2,'-U',infile3,'-S',outfile))
 }
-#5. sam to bam
+#4. sam to bam
 for (i in all_samples) {
   infile <- paste0(sam_data_path,i,'.sam')
   outfile <- paste0(bam_data_path,i,'.bam')
   system(paste('sbatch /storage/htc/birchlerlab/CENH3_ChIP-seq/scripts/bash_sub.sbatch samtools view -bSF4',
                infile,'>',outfile))
 }
-#6. merge bam
+#5. merge bam
 all_samples1 <- unique(gsub('_.+','',all_samples))
 for (i in all_samples1) {
   infile <- paste0(list.files(bam_data_path,pattern = paste0(i,'.+'),full.names = T),collapse = ' ')
@@ -77,4 +61,4 @@ for (i in all_samples1) {
                outfile,infile))
 }
 
-#7. MACS2 call peaks 
+#6. MACS2 call peaks 
