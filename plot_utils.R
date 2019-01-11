c8a9a5bd038712603e0f9107b188f744bc6f7599
Chris Chen <chen08900@gmail.com>
2019-01-11 12:15:44 -0600

    update plot utils

--- plot_utils.R
+++ plot_utils.R
@@ -1,3 +1,22 @@
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  plots <- c(list(...), plotlist)
+  numPlots = length(plots)
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  if (numPlots==1) {
+    print(plots[[1]])
+  } else {
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    for (i in 1:numPlots) {
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 plot_distribution = function(ratio , title_name ,left_line,right_line,max_ratio = 6,text_size=8){
   ratio[ratio>max_ratio]=max_ratio
   library(ggplot2)
@@ -309,24 +328,7 @@ get_normal_pvalue <- function(x,test_desc){
   return(res1)
 }
 
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-  plots <- c(list(...), plotlist)
-  numPlots = length(plots)
-  if (is.null(layout)) {
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  if (numPlots==1) {
-    print(plots[[1]])
-  } else {
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    for (i in 1:numPlots) {
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,layout.pos.col = matchidx$col))
-    }
-  }
-}
+
 
 plot_distribution_pairs <- function(r,cis_genes,title_name = '',left_line = 0.67,right_line = 1.5,max_ratio = 6){
   g <- multiplot(plotlist = list(plot_distribution(r[cis_genes],title_name = 'Cis genes',left_line = left_line,right_line = right_line,max_ratio = max_ratio),
